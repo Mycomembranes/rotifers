df708f4c8f6a0c69d49514796b5cc6bb3094346c
Mukshud Ahamed <82729894+Mycomembranes@users.noreply.github.com>
2025-06-04 13:16:29 -0400

    Fix failing log test

--- dev/test/log_test.py
+++ dev/test/log_test.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python3
-from rotifer.core.log import log
 import unittest
+from io import StringIO
+import sys
+from rotifer.core.log import log
 
 
 class LogTestCase(unittest.TestCase):
 
-    def test1(self):
-        self.assertEqual('a' ,'b',msg = 'return should be a nor b')
+    def test_log_outputs_message(self):
+        buf = StringIO()
+        stderr = sys.stderr
+        sys.stderr = buf
+        try:
+            log({1: 'test message'}, level=1)
+        finally:
+            sys.stderr = stderr
+        self.assertIn('test message', buf.getvalue())
 
 if __name__ == '__main__':
     unittest.main()
